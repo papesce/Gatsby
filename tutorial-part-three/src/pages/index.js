da600c580cd91c4f4fb2e2d1e0ff21d6d0cfdec3
Pablo Pesce <papesce@gmail.com>
2018-01-20 17:54:27 -0300

    part four

---
+++ tutorial-part-three/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1>Hi ! I'm building a fate Gats</h1>
+    <p>What do I like to do? Lots of course but definitely enjoy building
+      websites.</p>
+  </div>
+);
