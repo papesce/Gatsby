119699c262d895e2d735538e0d0eaf51941a80df
Pablo Pesce <papesce@gmail.com>
2018-01-20 19:02:59 -0300

    gatsby part four

---
+++ tutorial-part-four/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "bal bal bal"
+  },
+  plugins: [
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+};
